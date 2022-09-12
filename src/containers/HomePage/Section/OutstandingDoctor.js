ab70f57ed620d9159b84d9bd4a3b07464bbd891b
Hieult <infor.letrunghieu@gmail.com>
2022-09-12 14:30:14 +0700

    render data, language - render data OutstandingDoctor    + userService: getTopDoctorHomeService    + create actitonTypes    + adminAction: create fetchTopDoctor    + adminReducer: create case - change data language section OutstandingDoctor

--- src/containers/HomePage/Section/OutstandingDoctor.js
+++ src/containers/HomePage/Section/OutstandingDoctor.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
 import "./OutstandingDoctor.scss";
+import * as actions from "../../../store/actions";
+import { LANGUAGES } from "../../../utils";
 
 import Slider from "react-slick";
 
 class OutstandingDoctor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctors: [],
+        };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+            this.setState({
+                arrDoctors: this.props.topDoctorsRedux,
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.props.loadTopDoctors();
+    }
+
     render() {
+        let arrDoctors = this.state.arrDoctors;
+        let { language } = this.props;
         return (
             <div className="section outstanding-doctor">
                 <div className="section-container">
@@ -17,60 +41,43 @@ class OutstandingDoctor extends Component {
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="section-item">
-                                <div className="section-item-content">
-                                    <div className="outstanding-doctor-img"></div>
-                                    <div className="item-text">
-                                        <h3>Học vị, tên</h3>
-                                        <h4>Khoa</h4>
-                                    </div>
-                                </div>
-                            </div>
+                            {arrDoctors &&
+                                arrDoctors.length > 0 &&
+                                arrDoctors.map((item, index) => {
+                                    let imageBase64 = "";
+                                    if (item.image) {
+                                        imageBase64 = new Buffer(
+                                            item.image,
+                                            "base64"
+                                        ).toString("binary");
+                                    }
+                                    let nameVi = `${item.positionData.valueVi}, ${item.firstName} ${item.lastName}`;
+                                    let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
+                                    return (
+                                        <div
+                                            className="section-item"
+                                            key={index}
+                                        >
+                                            <div className="section-item-content">
+                                                <div
+                                                    className="outstanding-doctor-img"
+                                                    style={{
+                                                        backgroundImage: `url(${imageBase64})`,
+                                                    }}
+                                                ></div>
+                                                <div className="item-text">
+                                                    <h3>
+                                                        {language ===
+                                                        LANGUAGES.VI
+                                                            ? nameVi
+                                                            : nameEn}
+                                                    </h3>
+                                                    <h4>Khoa</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
                         </Slider>
                     </div>
                 </div>
@@ -81,12 +88,16 @@ class OutstandingDoctor extends Component {
 
 const mapStateToProps = (state) => {
     return {
+        language: state.app.language,
         isLoggedIn: state.user.isLoggedIn,
+        topDoctorsRedux: state.admin.topDoctors,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+    return {
+        loadTopDoctors: () => dispatch(actions.fetchTopDoctor()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(OutstandingDoctor);
