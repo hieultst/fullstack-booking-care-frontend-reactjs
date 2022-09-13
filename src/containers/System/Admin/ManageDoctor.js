55be87b9a952c284834cb7bdd1da5ba9cae597e6
Hieult <infor.letrunghieu@gmail.com>
2022-09-13 11:54:03 +0700

    libraries - install react-select - craete ManageDoctor section

---
+++ src/containers/System/Admin/ManageDoctor.js
@@ -0,0 +1,113 @@
+import React, { Component, Fragment } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+import Select from "react-select";
+
+import "./ManageDoctor.scss";
+import * as actions from "../../../store/actions";
+
+const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+];
+
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+class ManageDoctor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contentMarkdown: "",
+            contentHTML: "",
+            selectedDoctor: "",
+            description: "",
+        };
+    }
+
+    componentDidMount() {}
+
+    componentDidUpdate(prevProps, prevState, snapshot) {}
+
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            contentMarkdown: text,
+            contentHTML: html,
+        });
+    };
+
+    handleSaveContentMarkdown = () => {
+        console.log("check state: ", this.state);
+    };
+
+    handleChange = (selectedDoctor) => {
+        this.setState({ selectedDoctor }, () =>
+            console.log(`Option selected:`, this.state.selectedDoctor)
+        );
+    };
+
+    handleOnChangeDesc = (event) => {
+        this.setState({
+            description: event.target.value,
+        });
+    };
+
+    render() {
+        let arrUsers = this.state.userRedux;
+        return (
+            <div className="manage-doctor-container">
+                <div className="manage-doctor-title">Thêm thông tin bác sĩ</div>
+                <div className="more-info">
+                    <div className="content-left form-group">
+                        <label>Chọn bác sĩ</label>
+                        <Select
+                            value={this.state.selectedDoctor}
+                            onChange={this.handleChange}
+                            options={options}
+                        />
+                    </div>
+                    <div className="content-right form-group">
+                        <label>Thông tin giới thiệu: </label>
+                        <textarea
+                            className="form-control"
+                            rows="4"
+                            onChange={(event) => this.handleOnChangeDesc(event)}
+                            value={this.state.description}
+                        ></textarea>
+                    </div>
+                </div>
+                <div className="manage-doctor-editor">
+                    <MdEditor
+                        style={{ height: "500px" }}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={this.handleEditorChange}
+                    />
+                </div>
+                <button
+                    className="btn btn-primary save-content-doctor"
+                    onClick={() => this.handleSaveContentMarkdown()}
+                >
+                    Lưu thông tin
+                </button>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        users: state.admin.users,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchAllUserStart: () => dispatch(actions.fetchAllUserStart()),
+        deleteAUserRedux: (id) => dispatch(actions.deleteAUser(id)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageDoctor);
